e90ae8548910a67100331db3d4c7bbb42aaa4a14
oleg <oleg.kontsograda@sperrodigital.com>
2023-10-28 22:41:42 -0400

    fixing hero buttons centering

--- app/components/SermonsOverview.js
+++ app/components/SermonsOverview.js
@@ -3,31 +3,31 @@ import React from 'react'
 function SermonsOverview() {
     return (
 
-        <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-            <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
-                <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-gray-600">Recent Media Archives</h2>
+        <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+            <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+                <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-gray-600">Recent Media Archives</h2>
             </div>
 
-            <div class="grid grid-cols-2 md:grid-cols-3 gap-8 md:gap-12">
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-8 md:gap-12">
                 
-                <div class="text-center">
-                    <img class="rounded-xl w-[80%] mx-auto" src="https://theblazingcenter.com/wp-content/uploads/2018/11/Bible.png" alt="Recent Media Archive" />
-                    <div class="mt-2 sm:mt-4">
-                        <h3 class="text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-gray-600">
+                <div className="text-center">
+                    <img className="rounded-xl w-[80%] mx-auto" src="https://theblazingcenter.com/wp-content/uploads/2018/11/Bible.png" alt="Recent Media Archive" />
+                    <div className="mt-2 sm:mt-4">
+                        <h3 className="text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-gray-600">
                             Sermon Title
                         </h3>
-                        <p class="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-gray-400">
+                        <p className="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-gray-400">
                             Date:
                         </p>
                     </div>
                 </div>
-                <div class="text-center">
-                    <img class="rounded-xl w-[80%] mx-auto" src="https://theblazingcenter.com/wp-content/uploads/2018/11/Bible.png" alt="Recent Media Archive" />
-                    <div class="mt-2 sm:mt-4">
-                        <h3 class="text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-gray-600">
+                <div className="text-center">
+                    <img className="rounded-xl w-[80%] mx-auto" src="https://theblazingcenter.com/wp-content/uploads/2018/11/Bible.png" alt="Recent Media Archive" />
+                    <div className="mt-2 sm:mt-4">
+                        <h3 className="text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-gray-600">
                             Sermon Title
                         </h3>
-                        <p class="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-gray-400">
+                        <p className="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-gray-400">
                             Date:
                         </p>
                     </div>
--- app/page.jsx
+++ app/page.jsx
@@ -21,11 +21,11 @@ export default function Home() {
             <div className='text-white text-2xl'>Welcome to</div>
             <h1 className="flex py-4 text-3xl font-bold text-white sm:text-4xl">Bethesda Ukrainian Gospel&nbsp;Church</h1>
             <p className="py-6 text-lg text-white max-w-xs m-auto">"The LORD is my light and my salvation; whom shall I fear? <br />The LORD is the stronghold of my life; of whom shall I be afraid?"</p>
-            <div className="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
-              <a className="mt-[25%] w-[150px] sm:w-auto inline-flex justify-center items-center gap-x-3.5 text-center bg-white shadow-sm text-sm font-medium rounded-md hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-slate-900 transition py-3 px-4" href="https://github.com/htmlstreamofficial/preline/tree/main/examples/html" target="_blank">
+            <div className="mt-5 flex justify-center items-center gap-2 flex-col lg:flex-row sm:gap-3">
+              <a className="lg:mt-[25%] w-[150px] inline-flex justify-center items-center gap-x-3.5 text-center bg-white shadow-sm text-sm font-medium rounded-md hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-slate-900 transition py-3 px-4" href="https://github.com/htmlstreamofficial/preline/tree/main/examples/html" target="_blank">
                 Get in Touch
               </a>
-              <a className="w-[150px] sm:w-auto inline-flex justify-center items-center gap-x-3.5 text-center border-2 border-white-600 shadow-sm text-sm font-medium rounded-md text-white hover:bg-white hover:text-gray-500 focus:outline-none focus:ring-offset-gray-800 transition py-3 px-4" href="../examples.html">
+              <a className="lg:mt-[25%] w-[150px] inline-flex justify-center items-center gap-x-3.5 text-center border-2 border-white-600 shadow-sm text-sm font-medium rounded-md text-white hover:bg-white hover:text-gray-500 focus:outline-none focus:ring-offset-gray-800 transition py-3 px-4" href="../examples.html">
                 Recent Services
               </a>
             </div>
