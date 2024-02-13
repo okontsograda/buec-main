cc600f5c909c1a6411f0ebbf950018d28a470ed6
Oleg Kontsograda <oleg.kontsograda@gmail.com>
2024-02-12 21:25:29 -0500

    updating form submission

---
+++ app/utils/db.js
@@ -0,0 +1,20 @@
+import { MongoClient, ObjectId } from 'mongodb';
+
+const uri = 'mongodb+srv://buecadmin:Liv3123@buec.kzvfikj.mongodb.net/?retryWrites=true&w=majority';
+
+let cachedClient = null;
+
+export async function connectToDatabase() {
+  if (cachedClient) {
+    return cachedClient;
+  }
+
+  const client = await MongoClient.connect(uri);
+
+  cachedClient = client;
+  return client;
+}
+
+export function ObjectID() {
+  return Object
+}
