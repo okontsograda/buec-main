63157a672ba874f07475419c4199a66cd1cd4a7d
oleg <oleg.kontsograda@sperrodigital.com>
2024-06-13 09:23:08 -0400

    updating kidscamp kids page

--- app/kidscamp/kids/page.jsx
+++ app/kidscamp/kids/page.jsx
@@ -5,6 +5,7 @@ import { differenceInYears } from 'date-fns';
 const KidsCamp = () => {
     const [childrenData, setChildrenData] = useState(null);
     const [ageGroupsCount, setAgeGroupsCount] = useState({});
+    const [sortConfig, setSortConfig] = useState({ key: 'ageInt', direction: 'asc' });
 
     useEffect(() => {
         const fetchData = async () => {
@@ -49,7 +50,7 @@ const KidsCamp = () => {
                     ageInt: differenceInYears(new Date(), new Date(child.age)),
                 }));
 
-                // Sort children based on age in ascending order
+                // Sort children based on age in ascending order initially
                 childrenWithAge.sort((a, b) => a.ageInt - b.ageInt);
 
                 setChildrenData(childrenWithAge);
@@ -62,26 +63,98 @@ const KidsCamp = () => {
         fetchData();
     }, []);
 
+    const handleSort = (key) => {
+        let direction = 'asc';
+        if (sortConfig.key === key && sortConfig.direction === 'asc') {
+            direction = 'desc';
+        }
+        setSortConfig({ key, direction });
+
+        const sortedData = [...childrenData].sort((a, b) => {
+            if (a[key] < b[key]) {
+                return direction === 'asc' ? -1 : 1;
+            }
+            if (a[key] > b[key]) {
+                return direction === 'asc' ? 1 : -1;
+            }
+            return 0;
+        });
+
+        setChildrenData(sortedData);
+        console.log(childrenData)
+
+    };
+
     return (
         <>
-            <div className="flex flex-wrap justify-center mb-4">
-                {Object.keys(ageGroupsCount).map((age, idx) => (
-                    <div key={idx} className="bg-white rounded-lg text-center p-4 mb-4 w-1/5 sm:w-1/4 md:w-1/5 lg:w-1/6">
-                        <p className="text-lg font-semibold">{age}</p>
-                        <p className="text-xs text-gray-600">{ageGroupsCount[age]}</p>
-                    </div>
-                ))}
+            <div className="mx-auto lg:max-w-2xl">
+                <div className="divide-y divide-gray-100 border rounded-xl m-4 px-4">
+                    <details className="group" closed="true">
+                        <summary
+                            className="flex cursor-pointer list-none items-center justify-between py-4 text-lg font-medium text-secondary-900 group-open:text-primary-500">
+                            Age Groups
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                                    stroke="currentColor" className="block h-5 w-5 group-open:hidden">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                                    stroke="currentColor" className="hidden h-5 w-5 group-open:block">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15" />
+                                </svg>
+                            </div>
+                        </summary>
+                        <div className="flex flex-wrap justify-center mb-4">
+                            {Object.keys(ageGroupsCount).map((age, idx) => (
+                                <div key={idx} className="bg-white rounded-lg text-center p-4 mb-4 w-1/5 sm:w-1/4 md:w-1/5 lg:w-1/6">
+                                    <p className="text-lg font-semibold">{age}</p>
+                                    <p className="text-xs text-gray-600">{ageGroupsCount[age]}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </details>
+                    <details className="group" closed="true">
+                        <summary
+                            className="flex cursor-pointer list-none items-center justify-between py-4 text-lg font-medium text-secondary-900 group-open:text-primary-500">
+                            Allergies
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                                    stroke="currentColor" className="block h-5 w-5 group-open:hidden">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                                    stroke="currentColor" className="hidden h-5 w-5 group-open:block">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15" />
+                                </svg>
+                            </div>
+                        </summary>
+                        <div className="mb-4">
+                            <div>
+                                {childrenData ? (
+                                    childrenData.map((child, idx) => (
+                                        <div key={idx}>
+                                        <div className='border-b p-2'>{child.childsLastName}, {child.childsFirstName} <span className='float-right'>{child.childsAllergies || "N/A"}</span></div>
+                                        </div>
+                                    ))
+                                ) : (
+                                    <div>Loading data...</div>
+                                )}
+                            </div>
+                        </div>
+                    </details>
+                </div>
             </div>
             <div className="overflow-x-auto">
                 <table className="min-w-full bg-white border text-sm border-gray-200 shadow-md">
                     <thead>
                         <tr className="bg-gray-100 text-left">
                             <th className="px-4 py-2 border">#</th>
-                            <th className="px-4 py-2 border">First Name</th>
-                            <th className="px-4 py-2 border">Last Name</th>
-                            <th className="px-4 py-2 border">Age</th>
+                            <th className="px-4 py-2 border cursor-pointer" onClick={() => handleSort('childsFirstName')}>First Name</th>
+                            <th className="px-4 py-2 border cursor-pointer" onClick={() => handleSort('childsLastName')}>Last Name</th>
+                            <th className="px-4 py-2 border cursor-pointer" onClick={() => handleSort('ageInt')}>Age</th>
                             <th className="px-4 py-2 border">Birthdate</th>
-                            <th className="px-4 py-2 border">Allergies</th>
+                            <th className="px-4 py-2 border">Check-in</th>
+                            <th className="px-4 py-2 border">Check-out</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -93,13 +166,14 @@ const KidsCamp = () => {
                                     <td className="px-4 py-2 border">{child.childsLastName}</td>
                                     <td className="px-4 py-2 border">{child.ageInt}</td>
                                     <td className="px-4 py-2 border">{child.age}</td>
-                                    <td className="px-4 py-2 border">{child.childsAllergies}</td>
+                                    <td className="px-4 py-2 border"></td>
+                                    <td className="px-4 py-2 border"></td>
                                 </tr>
                             ))
                         ) : (
                             <tr>
-                                <td colSpan="6" className="px-4 py-2 border text-center">
-                                    No children data available
+                                <td colSpan="7" className="px-6 py-8 border text-center">
+                                    Loading data...
                                 </td>
                             </tr>
                         )}
